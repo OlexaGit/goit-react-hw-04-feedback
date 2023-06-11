0b02504faf627d9352cc6fd013915b40d4297edb
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-06-11 11:55:29 +0300

    change class on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './Feedback/Feedback';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const onLeaveFeedback = item => {
+    setState(prevState => ({ ...prevState, [item]: state[item] + 1 }));
   };
 
-  onLeaveFeedback = item => {
-    this.setState(state => ({ [item]: state[item] + 1 }));
-  };
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((previousValue, number) => {
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((previousValue, number) => {
       return previousValue + number;
     }, 0);
   };
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((state.good * 100) / countTotalFeedback());
   };
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={state} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </div>
+  );
+};
